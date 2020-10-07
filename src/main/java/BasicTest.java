36715788050fccca8c5e444973801ec931f11669
功 陈 <jeffchen0620@163.com>
2020-10-07 16:20:21 +0800

    test: CHEN Gong testing again

--- src/main/java/BasicTest.java
+++ src/main/java/BasicTest.java
@@ -1,9 +1,10 @@
 public class BasicTest {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Shihang ZHOU testing");
         System.out.println("Xinyu Feng testing");
         System.out.println("GUO jiadong testing");
         System.out.println("Yuxuan ZONG testing");
         System.out.println("Gong CHEN testing");
+        System.out.println("Gong CHEN testing again");
     }
 }
