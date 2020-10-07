2231215a0f6d08a192f2e92ae551cac0c502a512
yzong12138 <yuxuan.zong@insa-lyon.fr>
2020-10-07 15:49:54 +0800

    Test(main):宗宇轩本地测试

--- src/main/java/BasicTest.java
+++ src/main/java/BasicTest.java
@@ -2,5 +2,6 @@ public class BasicTest {
     public static void main(String args[]) {
         System.out.println("Shihang ZHOU testing");
         System.out.println("Xinyu Feng testing");
+        System.out.println("Yuxuan ZONG testing");
     }
 }
