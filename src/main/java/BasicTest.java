17584b634d4b3a132afb3adea7a480cfebc683d3
功 陈 <jeffchen0620@163.com>
2020-10-07 16:04:21 +0800

    test:CHEN Gong testing

--- src/main/java/BasicTest.java
+++ src/main/java/BasicTest.java
@@ -1,5 +1,6 @@
 public class BasicTest {
     public static void main(String args[]) {
         System.out.println("Shihang ZHOU testing");
+        System.out.println("Gong CHEN testing");
     }
 }
