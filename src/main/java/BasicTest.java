893f013b6c7f68bbb41af3e8b70319092eb98e7f
P.R GUO <guojiadonghz@gmail.com>
2020-10-07 15:58:25 +0800

    Test(main):GJD本地测试成功
    Test(main):GJD本地测试成功

--- src/main/java/BasicTest.java
+++ src/main/java/BasicTest.java
@@ -2,7 +2,7 @@ public class BasicTest {
     public static void main(String args[]) {
         System.out.println("Shihang ZHOU testing");
         System.out.println("Xinyu Feng testing");
-        System.out.println("GUO jiadong testing");
+        System.out.println("GUO jiadong new testing");
         System.out.println("Yuxuan ZONG testing");
     }
 }
