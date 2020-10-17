542f4406df8eb761a72854f0a1f1ed4a1a92b8e0
yzong12138 <yuxuan.zong@insa-lyon.fr>
2020-10-17 22:10:38 +0800

    feat(IHM.ExportRoute): Add the logic of adding the words in the textArea to an file and then save it

--- src/main/java/IHM/ExportRoute.java
+++ src/main/java/IHM/ExportRoute.java
@@ -5,6 +5,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * The IHM which allow to export the route as XML
@@ -53,7 +56,25 @@ public class ExportRoute {
     class ExportActionListener implements ActionListener{
 
         public void actionPerformed(ActionEvent e){
+            JFileChooser jfc = new JFileChooser(".");
+            jfc.showSaveDialog(f);
+            File file = jfc.getSelectedFile();
 
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(file);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+
+            try {
+                fw.write(textArea.getText());
+                fw.close();
+                // System.out.println(textArea.getText());
+                // System.out.println(file);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         }
 
     }
