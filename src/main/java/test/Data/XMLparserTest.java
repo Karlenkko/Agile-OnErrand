4e32034f188bd0b78cbfd58cb3020b8a7b9f66b6
CHEN GONG <jeffchen0620@163.com>
2020-10-18 17:30:34 +0200

    feat(test): tests for XMLParser of map and requests.

--- src/main/java/test/Data/XMLparserTest.java
+++ src/main/java/test/Data/XMLparserTest.java
@@ -1,8 +1,17 @@
 package test.Data; 
 
-import org.junit.Test; 
+import Model.*;
+import org.junit.Test;
 import org.junit.Before; 
-import org.junit.After; 
+import org.junit.After;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import static Data.XMLparser.*;
+import static org.junit.Assert.assertEquals;
 
 /** 
 * XMLparser Tester. 
@@ -11,16 +20,20 @@ import org.junit.After;
 * @since <pre>Oct 18, 2020</pre> 
 * @version 1.0 
 */ 
-public class XMLparserTest { 
+public class XMLparserTest {
 
+    Map map;
+    Mission mission;
 @Before
-public void before() throws Exception { 
-} 
+public void init() {
+    map = new Map();
+    mission = new Mission();
+
+}
 
 @After
-public void after() throws Exception { 
-} 
-
+public void exit(){
+}
 /** 
 * 
 * Method: parserMap(Map map) 
@@ -28,8 +41,19 @@ public void after() throws Exception {
 */ 
 @Test
 public void testParserMap() throws Exception { 
-//TODO: Test goes here... 
-} 
+    parserMap(map);
+
+    HashMap<Long, Intersection> mapIntersections = map.getAllIntersections();
+    assertEquals(45.775486, mapIntersections.get(Long.parseLong("2684668925")).getLatitude(), 0.000001);
+    assertEquals(4.888253, mapIntersections.get(Long.parseLong("2684668925")).getLongitude(), 0.000001);
+
+    LinkedList<Segment> mapSegments = map.getAllSegments();
+    assertEquals(Long.parseLong("2509481775"), mapSegments.get(0).getDestination().getId(), 0);
+    assertEquals(Long.parseLong("2684668925"), mapSegments.get(0).getOrigin().getId(), 0);
+    assertEquals("Rue Château-Gaillard", mapSegments.get(0).getName());
+    assertEquals(97.249695, mapSegments.get(0).getLength(), 0.000001);
+
+}
 
 /** 
 * 
@@ -37,9 +61,21 @@ public void testParserMap() throws Exception {
 * 
 */ 
 @Test
-public void testParserRequest() throws Exception { 
-//TODO: Test goes here... 
-} 
+public void testParserRequest() throws Exception {
+    parserMap(map);
+    parserRequest(mission, map);
+
+    ArrayList<Request> missionRequests = mission.getAllRequests();
+    System.out.println(missionRequests.get(0).getPickup());
+    assertEquals(1362781062, missionRequests.get(0).getPickup().getId(), 0);
+    assertEquals(27359745, missionRequests.get(0).getDelivery().getId(), 0);
+    assertEquals(540, missionRequests.get(0).getPickupDuration());
+    assertEquals(540, missionRequests.get(0).getDeliveryDuration());
+
+    Intersection missionDepot = mission.getDepot();
+    assertEquals(25610888, missionDepot.getId(), 0);
+
+}
 
 
 /** 
@@ -180,6 +216,6 @@ try {
 } catch(InvocationTargetException e) { 
 } 
 */ 
-} 
+}
 
-} 
+}
