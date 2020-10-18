b96710a74e870d58b70fb65b0466ae3352dc7c5c
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2020-10-18 21:11:32 +0200

    feat(Algorithm): add dijkstra many to many method, need to further figure TSP template structure

--- src/main/java/Algorithm/MapGraph.java
+++ src/main/java/Algorithm/MapGraph.java
@@ -2,16 +2,20 @@ package Algorithm;
 
 
 import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ManyToManyShortestPathsAlgorithm.ManyToManyShortestPaths;
 import org.jgrapht.alg.shortestpath.DijkstraManyToManyShortestPaths;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class MapGraph {
-    private Graph<Integer, DefaultWeightedEdge> g;
-    private HashMap<Integer, Integer> addressPriorities;
-    private DijkstraManyToManyShortestPaths<Integer, DefaultWeightedEdge> shortestPaths;
+    private Graph<Long, DefaultWeightedEdge> g;
+    private HashMap<Long, Long> addressPriorities;
+    private Set<Long> allAddresses;
+    private DijkstraManyToManyShortestPaths<Long, DefaultWeightedEdge> shortestPathAlgo;
+    private ManyToManyShortestPaths<Long, DefaultWeightedEdge> shortestPaths;
     /**
      * Constructor of MapGraph, creates an empty directed weighted map using the jgrapht library
      */
@@ -23,31 +27,44 @@ public class MapGraph {
      * Add an Intersection to the MapGraph using its id
      * @param intersectionId the id of the Intersection
      */
-    public void addVertex(int intersectionId) {
+    public void addVertex(long intersectionId) {
         g.addVertex(intersectionId);
     }
 
     /**
      * Set tour visiting priority of an address(Intersection)
      * 0 means delivery address, 1 means pickup address, 2 means depot address
-     * @param addressIntersectionId the id of the address Intersection
-     * @param priority the priority of the address Intersection
+     * @param pickupAddressId the id of the pickup address Intersection
+     * @param deliveryAddressId the id of the delivery address Intersection
      */
-    public void setAddressPriority(int addressIntersectionId, int priority) {
-        addressPriorities.putIfAbsent(addressIntersectionId, priority);
+    public void setAddressPriorities(long pickupAddressId, long deliveryAddressId) {
+        addressPriorities.putIfAbsent(pickupAddressId, deliveryAddressId);
     }
+
+    /**
+     * add depot address id, pickup address id and delivery address id to the set
+     * @param addressId the Intersection id of an address
+     */
+    public void addAddress(long addressId) {
+        allAddresses.add(addressId);
+    }
+
     /**
      * Add a Segment to the MapGraph without its name
      * @param sourceIntersectionId the origin/source Intersection id
      * @param targetIntersectionId the destination/target Intersection id
      * @param length the length of the Segment, expressed in meters
      */
-    public void addEdge(int sourceIntersectionId, int targetIntersectionId, double length) {
+    public void addEdge(long sourceIntersectionId, long targetIntersectionId, double length) {
         g.setEdgeWeight(sourceIntersectionId, targetIntersectionId, length);
     }
 
-//    public void calculateShortestPaths() {
-//        shortestPaths = new DijkstraManyToManyShortestPaths<>(g);
-//    }
+    /**
+     * Calculate the shortest paths among all pickup and delivery
+     */
+    public void calculateShortestPaths() {
+        shortestPathAlgo = new DijkstraManyToManyShortestPaths<>(g);
+        shortestPaths = shortestPathAlgo.getManyToManyPaths(allAddresses, allAddresses);
+    }
 }
 
