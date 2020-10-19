96d1ea1a0d9a30a3a6e0264aecfd8e5c842e0829
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2020-10-19 10:07:13 +0200

    feat(Algorithm): stop developping on jgrapht, use the TSP package

---
+++ src/main/java/Algorithm/TSP1.java
@@ -0,0 +1,17 @@
+package Algorithm;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class TSP1 extends TemplateTSP {
+	@Override
+	protected int bound(Integer currentVertex, Collection<Integer> unvisited) {
+		return 0;
+	}
+
+	@Override
+	protected Iterator<Integer> iterator(Integer currentVertex, Collection<Integer> unvisited, Graph g) {
+		return new SeqIter(unvisited, currentVertex, g);
+	}
+
+}
