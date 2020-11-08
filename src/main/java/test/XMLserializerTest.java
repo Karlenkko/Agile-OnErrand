f2d68a6458ea1ad543aad1a146ecd8e936276ce6
HZjiadong <guojiadonghz@gmail.com>
2020-11-08 10:41:30 +0800

    Create XMLserializerTest.java

---
+++ src/main/java/test/XMLserializerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLserializerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
